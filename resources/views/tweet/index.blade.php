7afc4c24e7a074ad027b7aca12c39c9b9e1f59f4
taroosg <root@taroosg.dev>
2023-09-19 20:36:40 +0900

    ユーザページと検索機能を追加

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -28,7 +28,9 @@
               <tr class="hover:bg-gray-lighter">
                 <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">
                   <div class="flex">
-                    <p class="text-left text-gray-800 dark:text-gray-200">{{$tweet->user->name}}</p>
+                    <a href="{{ route('follow.show', $tweet->user->id) }}">
+                      <p class="text-left text-gray-dark dark:text-gray-200">{{$tweet->user->name}}</p>
+                    </a>
                     <!-- follow 状態で条件分岐 -->
                     @if(Auth::user()->followings()->where('users.id', $tweet->user->id)->exists())
                     <!-- unfollow ボタン -->
